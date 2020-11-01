eb678eff32a24c21c6ef21145596c463d7e488fa
Erich Robbi <erich@LAPTOP-E3HNM0A7.homenet.telecomitalia.it>
2020-11-01 21:14:56 +0100

    const action inutilizzato, mappa intenti init 1 sola volta

--- src/api/handlers.js
+++ src/api/handlers.js
@@ -2,11 +2,9 @@
 
 module.exports = {
   welcomeHandler : function(agent){
-    console.log('welcome')
     return agent.add("You talkin’ to me?");
   },
   fallbackHandler : function(agent) {
-    console.log("fallback")
     agent.add(`I didn't understand`);
     agent.add(`I'm sorry, can you try again?`);
   }
--- src/server.js
+++ src/server.js
@@ -11,20 +11,18 @@ const handlers = require('../src/api/handlers.js');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+
+const intentMap = new Map();
+intentMap.set('Default Welcome Intent', handlers.welcomeHandler);
+intentMap.set('Default Fallback Intent', handlers.fallbackHandler);
+
 function Webhook(req, res) {
-  const agent = new WebhookClient({request: req, response: res});
-  console.log("asdf");
-  console.log('----------------------')
-  const action = agent.action;
-  let intentMap = new Map();
-  intentMap.set('Default Welcome Intent', handlers.welcomeHandler);
-  intentMap.set('Default Fallback Intent', handlers.fallbackHandler);
+  const agent = new WebhookClient({request:req,response:res});
   agent.handleRequest(intentMap);
 }
 
 // root del server
 app.post('/', function (req, res) {
-  console.log("asdf");
   // passo la richiesta alla funzione webhook
   Webhook(req, res);
 });
