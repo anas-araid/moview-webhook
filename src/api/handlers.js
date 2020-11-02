81daaa1761a0078b3de5c5c647b089b00193980c
asdf1899 <araid.anas99@gmail.com>
2020-11-02 13:13:30 +0100

    Add getMovieByYear in movieController.js

--- src/api/handlers.js
+++ src/api/handlers.js
@@ -1,7 +1,7 @@
 'use strict';
+const movieController = require('../api/movieControllers.js');
 
 module.exports = {
-  // con agent.parameters.risorsa si può accedere al parametro
   welcomeHandler : function(agent){
     agent.add("You talkin’ to me?");
     agent.add("Do you want a movie suggestion from specific actors, directors, genres, year, language? You can also provide keywords to further narrow down the research. \nFor example: give me an action movie from the 80s with Stallone")
@@ -10,4 +10,21 @@ module.exports = {
     agent.add("I didn't understand");
     agent.add("I'm sorry, can you try again?");
   }
+  /*
+  ########################################################################
+  # Con agent.parameters.risorsa si può accedere al parametro (in teoria)
+  #
+  # IMPORTANTE: fare il return della promise altrimenti agent.add non va
+  #
+  ########################################################################
+
+  altriHandler : function(agent){
+    let year = agent.parameters.year;
+    return movieController.getMovieByYear(year).then(res => {
+      console.log(res.data.results)
+      agent.add("lista film... " + res.data.results[0].original_title);
+    });
+  }
+  */
+
 }
--- src/api/movieControllers.js
+++ src/api/movieControllers.js
@@ -1,15 +1,16 @@
 'use strict';
 
-const axios = require('axios');
 require('dotenv').config();
+const axios = require('axios');
+const TMDB_APIKEY = process.env.TMDB_KEY;
 
 module.exports = {
-  //const TMDB_APIKEY = process.env.TMDB_KEY
   getMovieByGenre : function(genre){
-    // axios
+    
   },
   getMovieByYear : function(year){
-
+    let query = 'https://api.themoviedb.org/3/discover/movie?api_key='+TMDB_APIKEY+'&year='+year;
+    return axios.get(query);
   },
   getMovieByKeyword : function(keyword){
 
